815d8943fe7e694a44250d05cb5c204c1595f976
Ashu <abhishekaggarwal2801@gmail.com>
2024-03-02 13:30:37 +0530

    radius changes with height

--- animate.js
+++ animate.js
@@ -6,23 +6,28 @@ const c = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let diff = 0.5;
+const diff = 0.5;
+const beg = 6;
+const end = 105;
+const angle = 0.78;
+const speed = 0.01;
+
+function calcX(z){
+  return z * Math.sin(z);
+}
+
+function calcY(z){
+  return Math.abs(Math.sqrt(z * z * Math.cos(z) * Math.cos(z) + (1000 * 1000) / z) *
+  Math.sin(angle + Math.atan(1000 / Math.sqrt(z) / (z * Math.cos(z)))));
+}
+
+x1 = calcX(beg);
+y1 = calcY(beg);
+
+x2 = calcX(end);
+y2 = calcY(end);
+
 let dz = 0;
-let beg = 6;
-let end = 74.78;
-
-z = beg;
-x1 = z * Math.sin(z);
-y1 =
-  Math.sqrt(z * z * Math.cos(z) * Math.cos(z) + (1000 * 1000) / z) *
-  Math.sin(0.6 + Math.atan(1000 / Math.sqrt(z) / (z * Math.cos(z))));
-
-z = end;
-x2 = z * Math.sin(z);
-y2 =
-  Math.sqrt(z * z * Math.cos(z) * Math.cos(z) + (1000 * 1000) / z) *
-  Math.sin(0.6 + Math.atan(1000 / Math.sqrt(z) / (z * Math.cos(z))));
-
 function animate() {
   requestAnimationFrame(animate);
 
@@ -32,24 +37,23 @@ function animate() {
   c.stroke();
 
   for (let z = beg + dz; z < end; z += diff) {
-    x = (innerWidth / (2 * x2)) * (z * Math.sin(z)) + innerWidth / 2;
-    y =
-      Math.sqrt(z * z * Math.cos(z) * Math.cos(z) + (1000 * 1000) / z) *
-      Math.sin(0.6 + Math.atan(1000 / Math.sqrt(z) / (z * Math.cos(z))));
-    y = (innerHeight / (y1 - y2)) * (Math.abs(y) - y2);
-    rad = 10 + 3 * Math.cos(z);
+    x = (innerWidth / (2 * x2)) * calcX(z) + innerWidth / 2;
+    y = calcY(z);
+    y = (innerHeight / (y1 - y2)) * (y - y2) + innerHeight/10;
+    rad = (7 - 4 * Math.cos(z)) * ((end-z)/(end-beg));
+
+    var gradient = c.createRadialGradient(x, y, 0, x, y, rad);
+    gradient.addColorStop(0, "white");
+    gradient.addColorStop(1, "transparent");
 
     c.beginPath();
     c.arc(x, y, rad, 0, Math.PI * 2, false);
-    var gradient = c.createRadialGradient(x, y, 0, x, y, 2 * rad);
-    gradient.addColorStop(0, "white");
-    gradient.addColorStop(0.7, "transparent");
     c.fillStyle = gradient;
     c.fill();
     c.strokeStyle = "transparent";
     c.stroke();
   }
-  dz += 0.01;
+  dz += speed;
   if (dz > diff) dz = 0;
 }
 
